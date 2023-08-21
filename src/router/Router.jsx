2750799e7eec9c709e1f2f8e1bd092e1a1f34e02
kingkiboots <tuffhr4@gmail.com>
2023-08-21 17:49:16 +0900

    sidebar => navbar로 이름 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import App2 from "../App2";
 import App3 from "../App3";
-import Sidebar from "../common/sidebar/Sidebar";
+import Navbar from "../common/navbar/Navbar";
 import SampleForMemoizationLayout from "../layout/sampleForMemoization/SampleForMemoizationLayout";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" exact element={<Sidebar />}>
+      <Route path="/" exact element={<Navbar />}>
         <Route path="/" element={<App />} />
         <Route path="/app2" element={<App2 />} />
         <Route path="/app3" element={<App3 />} />
